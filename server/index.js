80d958ece83b0958e55002b620b722e4eab768ef
Taylor Blair <taylorblair1128@gmail.com>
2022-05-03 19:28:39 -0600

    fix

--- server/index.js
+++ server/index.js
@@ -1,17 +1,17 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const port = 3000
+// const express = require('express')
+// const cors = require('cors')
+// const app = express()
+// const port = 3000
 
-app.use(cors());
-app.use(express.json());
-app.use('/js', express.static(path.join(__dirname, 'public/main.js')))
+// app.use(cors());
+// app.use(express.json());
+// app.use('/js', express.static(path.join(__dirname, 'public/main.js')))
 
 
-app.get('/js', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/main.js'))
-  })
+// app.get('/js', (req, res) => {
+//     res.sendFile(path.join(__dirname, './public/main.js'))
+//   })
 
-app.listen(port, () => {
-    console.log(`Lab server listening at http://localhost:${port}`)
-  })
+// app.listen(port, () => {
+//     console.log(`Lab server listening at http://localhost:${port}`)
+//   })
