f345471a35fa2dbb93b3d54d1875c7ea3b4fa994
Taylor Blair <taylorblair1128@gmail.com>
2022-05-04 08:29:50 -0600

    fixeddddd

--- server/index.js
+++ server/index.js
@@ -3,8 +3,12 @@ const cors = require("cors");
 
 const app = express();
 
-let namesArr = ["Jeff", "Jim", "Node" ]
+app.get('/', (req, res) => {
+  res.send('Successful response.');
+});
 
-app.use(cors());
+app.get('/js', (req, res) => {
+  res.sendFile(path.join(__dirname, './server/index.js'))
+})
 
-app.use(express.json());
+app.listen(3000, () => console.log('Example app is listening on port 3000.'));
